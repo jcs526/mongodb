b150b6738ad4893750bdfc2829ce096a26e05c50
Chansol Jeong <mutongzang@gmail.com>
2024-02-12 23:46:11 +0900

    eslint 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,37 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    "eslint:recommended",
-  ],
+  extends: "eslint:recommended",
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: [
+        ".eslintrc.{js,cjs}"
+      ],
       parserOptions: {
-        sourceType: "script",
-      },
-    },
+        sourceType: "script"
+      }
+    }
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: "latest"
   },
   rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    quotes: ["error", "double"],
+    "quote-props": ["error", "as-needed"],
+    semi: ["error", "always"],
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "comma-spacing": ["error", { before: false, after: true }],
+    "comma-dangle": ["error", "only-multiline"],
+    "max-len": ["error", { code: 80, ignoreUrls: true }],
+    "object-curly-spacing": ["error", "always"],
+    "arrow-parens": ["error", "as-needed"],
     "prefer-const": "error",
-  },
+    "no-var": "error"
+  }
 };
